ea9e701cd789dfa44ae12116904ba6f519d6bacc
Colton Ogden <coltonoscopy@gmail.com>
2019-04-26 14:15:24 -0400

    initial commit

---
+++ script1.py
@@ -0,0 +1,69 @@
+# thechocokid21
+'''
+Written by Colton Ogden (cs50 Live) and Twitch Chat!
+
+Program is a Pomodore Script, should act as a timer for a short period of time,
+puncutated by short break periods.
+
+4/26/2019 Chat Codes Python (Part 3)
+'''
+# AMENDMENT: sarulcs
+if "userInput" == "quit":                               # TheBluePenguin9
+    # AMENDMENT: # The_True_Canise
+    quit()                                              # thelastkek
+import time                                             # Colton
+import cs50                                             # indrareddy5
+setattr(time, 'time', lambda: 42)                       # lgfrbcsgo
+import webbrowser                                       # badbayesian
+import datetime                                         # dibaloke
+
+# thelastkek
+# webbrowser.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+
+for n in range(9999999999999):                          # St3veR0nix
+    pass                                                # The_True_Canise
+
+repeats = 0                                             # rahul_maru
+# AMENDMENT: 1aayog, naclEric, badbayesian
+start_time = int(str(time.time()))                                      # andrej_petelin
+# AMENDMENT: asli_t
+break_time = int(input("length of break: "))                            # naclEric
+tomatoes_completed = 0                                                  # PokeoIntenso
+# AMENDMENT: magedrifaat, andrej_petelin
+work_time = int(input("Length of working time: ")) * 60                 # sm_raiyyan
+print(datetime.datetime.fromtimestamp(time.time()).strftime('%H:%M:%S'))# dibaloke                                    # The_True_Canise
+sleep(2)                                                                # indrareddy5
+checkmark = []                                                          # woxroox
+
+def pomodoro_clock(start_time, work_time):                              # js_coder
+    pass                                                                # sarulcs
+# AMENDMENT: # theJPguy
+while tomatoes_completed < 4:                                           # Perzival1312
+    # AMENDMENT: skywalker_pro
+    if int(start_time) + int(work_time) == time.time():                 # Perzival1312
+        pass                                                            # Perzival1312
+        print("You need to take a break")                               # js_coder    
+        time.sleep(break_time)                                          # Amir Qadir
+        start_time += work_time                                         # richer6605
+        print("Start working again.")                                   # magedrifaat
+    else:                                                               # atacadaosaopaulo
+        # AMENDMENT: successbux
+        print("You need to keep working")                               # TheBluePenguin9
+        tomatoes_completed += 1                                         # graysenslade
+
+
+# dibaloke
+# Perzival1312
+# asli_t
+# andrej_petelin
+# badbayesian
+# atacadaosaopaulo
+# richer6605
+# js_coder
+# endgame_cs50
+# magedrifaat
+# UnlimitedBytes
+# sarulcs
+# thelastkek
+# The_True_Canise
+# graysenslade
